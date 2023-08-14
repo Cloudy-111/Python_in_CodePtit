359ced896698bdf89282b799851982ee35d6b514
Diep <diepani303@gmail.com>
2023-08-14 22:16:21 +0700

    ICPC0108

---
+++ Sum triple zero.py
@@ -0,0 +1,26 @@
+def countTriple(arr):
+    res = 0
+    n = len(arr)
+    for i in range(n-2):
+        left = i+1
+        right = n-1
+
+        while left < right:
+            sum = arr[i]+arr[left]+arr[right]
+            if sum == 0:
+                res += 1
+                left += 1
+            elif sum < 0:
+                left += 1
+            else:
+                right -= 1
+    return res
+
+
+t = int(input())
+while (t > 0):
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    arr.sort()
+    print(countTriple(arr))
+    t -= 1
