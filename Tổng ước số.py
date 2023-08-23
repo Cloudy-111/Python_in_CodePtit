731afcbaead6648dec54dca2074a6bcf0149ce92
Diep <diepani303@gmail.com>
2023-08-23 08:38:07 +0700

    PY01074

--- Tổng ước số.py
+++ Tổng ước số.py
@@ -1,24 +1,22 @@
-import sys
-
-
-def ptich(n):
-    sum = 0
-    i = 2
-    while n > 1:
-        while n % i == 0:
-            n //= i
-            sum += i
-        i += 1
-    return sum
-
-
-t = int(input())
-i = 0
-res = 0
-for line in sys.stdin:
-    n = int(line)
-    res += ptich(n)
+import math
+prime = [1]*2000001
+prime[0] = prime[1] = 0
+i = 2
+while i <= math.sqrt(2000001):
+    if prime[i] == 1:
+        for j in range(i*i, 2000001, i):
+            if prime[j] == 1:
+                prime[j] = i
     i += 1
-    if i == t:
-        break
+for j in range(2, 2000001):
+    if prime[j] == 1:
+        prime[j] = j
+t = int(input())
+res = 0
+while t > 0:
+    n = int(input())
+    while n > 1:
+        res += prime[n]
+        n //= prime[n]
+    t -= 1
 print(res)
