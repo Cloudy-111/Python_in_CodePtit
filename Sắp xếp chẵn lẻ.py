3d5d355327f9b0a70790ee2d1435cbf2880c87bf
Diep <diepani303@gmail.com>
2023-09-18 21:46:59 +0700

    PY02012

--- Sắp xếp chẵn lẻ.py
+++ Sắp xếp chẵn lẻ.py
@@ -1,24 +1,17 @@
-n = int(input())
-odd = []
-even = []
-arr = []
 # vl, đề là ghi đủ n số trong các dòng tiếp theo chứ không phải là nhập n số trong 1 dòng
+n = int(input())
+arr = []
 while 1:
     a = [int(x) for x in input().split()]
     arr += a
     if len(arr) == n:
         break
-for i in arr:
+e = [x for x in arr if x % 2 == 0].sort(reverse=True)
+o = [x for x in arr if x % 2 != 0].sort()
+for i in a:
     if i % 2:
-        odd.append(i)
+        print(o[-1])
+        o.pop()
     else:
-        even.append(i)
-even.sort(reverse=True)
-odd.sort()
-for i in range(n):
-    if arr[i] % 2:
-        print(odd[-1], end=' ')
-        odd.pop()
-    else:
-        print(even[-1], end=' ')
-        even.pop()
+        print(e[-1])
+        e.pop()
